71d73399bfe905f24a145c3c2e4229c1724f7f77
Alexander KK <alexkruckkneip@gmail.com>
2023-09-12 17:17:36 +0100

    Adds the loader function to the main.js file

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,3 +1,57 @@
 // ---------- ---------- ---------- ---------- ----------
 // Loader
 // ---------- ---------- ---------- ---------- ----------
+
+
+// Fade Out function
+// ---------- ---------- ---------- ---------- ----------
+function fadeOut(ms, el) {
+  var opacity  = 1,
+      interval = 50,
+      gap      = interval / ms;
+
+  function func() {
+    opacity -= gap;
+    el.style.opacity = opacity;
+
+    if(opacity <= 0) {
+      window.clearInterval(fading);
+      el.style.display = 'none';
+    }
+  }
+  var fading = window.setInterval(func, interval);
+}
+
+// Define transition function
+// ---------- ---------- ---------- ---------- ----------
+function loaded() {
+  // Define our variables
+  var docBody  = document.body,
+      anchors  = document.getElementsByTagName('a');
+
+  // Add loaded class to body
+  docBody.className += ' loaded';
+
+  // For each link
+  for (var i = 0; i < anchors.length; i++) {
+
+    // Click function
+    anchors[i].onclick = function(e) {
+
+      // Cache our anchor
+      var el = this;
+
+      // Stop default action
+      e.preventDefault();
+
+      // Fade out body
+      fadeOut(150, docBody);
+
+      // Change page after fade out
+      setTimeout( function() { location = el.href }, 150 );
+    }
+  }
+}
+
+// Fire transition function
+window.onload = loaded;
