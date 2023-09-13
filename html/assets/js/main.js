da21396a241d456f93ee46cf4bf2d8fc57075670
Alexander KK <alexkruckkneip@gmail.com>
2023-09-13 13:05:38 +0100

    Moving the percentage loader js to main.js

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -2,56 +2,37 @@
 // Loader
 // ---------- ---------- ---------- ---------- ----------
 
+/*!
+ * Percent-Preloader JS - v1
+ * @author JDM Digital - https://jdmdigital.co
+ * Copyright (c) 2022
+ */
+var counting = setInterval(function () {
+	var loader = document.getElementById("percentage");
+	var currval = parseInt(loader.innerHTML);
+	var Width = 99 - currval;
+	var loadscreen = document.getElementById("loader-progress");
+	loader.innerHTML = ++currval;
+	if (currval > 89){
+		loader.innerHTML = 90;
+		if(window.jQuery) {
+			//console.log('jquery loaded');
+			loader.innerHTML = 95;
+			if(document.readyState == "interactive") {
+				loader.innerHTML = 99;
+			}
+			if(document.readyState == "complete") {
+				//console.log('fully loaded!');
+				clearInterval(counting);
+				loader.innerHTML = 100;
+				jQuery("body").toggleClass('loaded');
+				setTimeout(function() {
+					jQuery('nav').css('visibility','visible')
+				}, 880);
+			}
+		}
+	}
 
-// Fade Out function
-// ---------- ---------- ---------- ---------- ----------
-function fadeOut(ms, el) {
-  var opacity  = 1,
-      interval = 50,
-      gap      = interval / ms;
-
-  function func() {
-    opacity -= gap;
-    el.style.opacity = opacity;
-
-    if(opacity <= 0) {
-      window.clearInterval(fading);
-      el.style.display = 'none';
-    }
-  }
-  var fading = window.setInterval(func, interval);
-}
-
-// Define transition function
-// ---------- ---------- ---------- ---------- ----------
-function loaded() {
-  // Define our variables
-  var docBody  = document.body,
-      anchors  = document.getElementsByTagName('a');
-
-  // Add loaded class to body
-  docBody.className += ' loaded';
-
-  // For each link
-  for (var i = 0; i < anchors.length; i++) {
-
-    // Click function
-    anchors[i].onclick = function(e) {
-
-      // Cache our anchor
-      var el = this;
-
-      // Stop default action
-      e.preventDefault();
-
-      // Fade out body
-      fadeOut(150, docBody);
-
-      // Change page after fade out
-      setTimeout( function() { location = el.href }, 150 );
-    }
-  }
-}
-
-// Fire transition function
-window.onload = loaded;
+	loadscreen.style.transition = "0.15s";
+	loadscreen.style.width = Width + "%";
+}, 20);
